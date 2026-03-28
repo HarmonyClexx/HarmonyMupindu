fe2ca1294355c01d82921ed5af05d4ca45843ac6
HarmonyClexx <harmonymupindu@gmail.com>
2026-03-28 19:22:59 +0200

    adjust paths

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type {Project, ContactForm} from '../types';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL as string});
 
 export const fetchProjects = () => API.get<{ data: Project[] }>('/projects');
 export const sendEmail = (formData: ContactForm) => API.post('/contact', formData);
